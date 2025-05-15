9b28bbbb7fc58b842fde56074d798ac509194ea9
veronikavatsyak <veronikateslya@ukr.net>
2025-05-15 18:00:30 -0400

    creating post transaction controller

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -1,5 +1,9 @@
-import { getAllTransactions } from '../services/transactions.js';
+import {
+  getAllTransactions,
+  createTransaction,
+} from '../services/transactions.js';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { RES_MSG } from '../constants/res-msg';
 
 export const getAllTransactionsController = async (req, res) => {
   const { page, perPage } = parsePaginationParams(req.query);
@@ -16,5 +20,16 @@ export const getAllTransactionsController = async (req, res) => {
     status: 200,
     message: 'Successfully found all transactions',
     data: transactions,
+  });
+};
+
+export const createTransactionController = async (req, res) => {
+  const userId = req.user.id;
+  const transaction = await createTransaction({ ...req.body, userId });
+  const { addTransaction } = RES_MSG[201];
+  res.status(201).json({
+    status: 201,
+    message: addTransaction,
+    data: transaction,
   });
 };
