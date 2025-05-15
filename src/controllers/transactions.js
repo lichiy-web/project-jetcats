aa010b4e2e72453389ae135d4599bb1b2caab1e8
frirw <jasperbmg@gmail.com>
2025-05-16 01:27:49 +0300

    Update transactions.js

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -1,4 +1,5 @@
 import { getAllTransactions } from '../services/transactions.js';
+import { updateTransaction } from '../../services/transactions.js';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 
 export const getAllTransactionsController = async (req, res) => {
@@ -16,5 +17,27 @@ export const getAllTransactionsController = async (req, res) => {
     status: 200,
     message: 'Successfully found all transactions',
     data: transactions,
+  });
+};
+
+export const patchTransactionController = async (req, res) => {
+  const { transactionId } = req.params;
+  const userId = req.user._id;
+  const updateData = req.body;
+
+  const updatedTransaction = await updateTransaction({
+    transactionId,
+    userId,
+    updateData,
+  });
+
+  if (!updatedTransaction) {
+    throw createHttpError(404, 'Transaction not found');
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: 'Transaction updated successfully',
+    data: updatedTransaction,
   });
 };
