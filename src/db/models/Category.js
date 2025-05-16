e5b4046194e6714a4d7eb3c953bcbd01b891b9b2
Yevhen Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-05-16 15:33:06 +0300

    remove userId field from the categorySchema

--- src/db/models/Category.js
+++ src/db/models/Category.js
@@ -10,11 +10,6 @@ const categorySchema = new Schema({
     type: String,
     required: true,
   },
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
 });
 
 const categoriesCollection = model('category', categorySchema);
