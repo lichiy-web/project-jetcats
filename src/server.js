3da5d774901b2c74e1f72026632863cb8fb7b492
Svitlana <160290680+Svitlana-2020@users.noreply.github.com>
2025-05-16 12:07:35 +0100

    routeCurrentUser

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
 import { swaggerDocs } from './middlewares/swaggerDocs.js';
+import currentRouter from './routers/current.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -30,6 +31,7 @@ export const setupServer = () => {
   app.use('/api-docs', swaggerDocs());
 
   app.use(router);
+  app.use(currentRouter);
 
   app.use(notFoundHandler);
 
