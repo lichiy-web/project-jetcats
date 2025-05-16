46ad086e5c12940f57c08659afbc04b0c4071727
Svitlana <160290680+Svitlana-2020@users.noreply.github.com>
2025-05-16 10:28:56 +0100

    updatedCollection

--- src/db/models/Category.js
+++ src/db/models/Category.js
@@ -1,26 +1,21 @@
 import { model, Schema } from 'mongoose';
-import { typeList, expenseCategories, incomeCategory } from '../../constants/categories';
 
 const categorySchema = new Schema({
   type: {
     type: String,
-    enum: typeList,
+    enum: ['income', 'expense'],
     required: true,
   },
   name: {
     type: String,
     required: true,
-    validate: {
-      validator: function (value) {
-        if (this.type === 'income') {
-          return incomeCategory.includes(value);
-        } else if (this.type === 'expense') {
-          return expenseCategories.includes(value);
-        }
-        return false;
-      },
-  },}}); 
-  
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
 
-const categoriesCollection = model('Category', categorySchema);
+const categoriesCollection = model('category', categorySchema);
 export default categoriesCollection;
