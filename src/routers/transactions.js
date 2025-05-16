7573477a8d4ed5698fc315031cd9d1e3f23970a9
NormalOne <149543061+Fedir-M@users.noreply.github.com>
2025-05-16 14:57:01 -0400

    created router.delete() for transaction

--- src/routers/transactions.js
+++ src/routers/transactions.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
 import { authenticate } from '../middlewares/authenticate.js';
+import { isValidId } from '../middlewares/isValidId.js';
 import {
   createTransactionController,
   deleteTransactionController,
@@ -11,6 +12,7 @@ import {
 } from '../controllers/transactions.js';
 
 const router = new Router();
+router.use(authenticate);
 /**
  * Це просто заглушка, щоб лінтер не лаявся на невикористаний validateBody та
  * контролери. Після створення всіх ріутів буде видалено.
@@ -26,6 +28,12 @@ router.post(
 
 // Нижче код раутів.
 
-router.get('/summary', authenticate, ctrlWrapper(getSummaryController));
+router.get('/summary', ctrlWrapper(getSummaryController));
+
+router.delete(
+  '/transactions/:transactionId',
+  isValidId(),
+  ctrlWrapper(deleteTransactionController),
+);
 
 export default router;
