3c64d9fbf7b794806837135434d25938491474da
frirw <jasperbmg@gmail.com>
2025-05-16 22:54:54 +0300

    Update transactions.js

--- src/services/transactions.js
+++ src/services/transactions.js
@@ -1,4 +1,5 @@
 import { TransactionCollection } from '../db/models/Transaction.js';
+import { UserCollection } from '../../db/models/User.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
 export const getAllTransactions = async ({
@@ -30,12 +31,65 @@ export const deleteTransaction = async ({ userId, transactionId }) => {
 };
 
 export const updateTransaction = async ({
-  userId,
   transactionId,
+  userId,
   updateData,
-  options = {},
 }) => {
-  // доповнити код-заглушку
+  const previousTransaction = await TransactionCollection.findOne({
+    _id: transactionId,
+    userId,
+  });
+  if (!previousTransaction) {
+    return null;
+  }
+
+  const user = await UserCollection.findById(userId);
+  if (!user) {
+    return null;
+  }
+
+  // сума збереженої транзакції з урахуванням знаку (від'ємна для видатків, та позитивна для доходів)
+  let previousAmount;
+  if (previousTransaction.type === 'income') {
+    previousAmount = previousTransaction.sum;
+  } else {
+    previousAmount = -previousTransaction.sum;
+  }
+
+  let newType;
+  if ('type' in updateData) {
+    newType = updateData.type;
+  } else {
+    newType = previousTransaction.type;
+  }
+
+  let newSum;
+  if ('sum' in updateData) {
+    newSum = updateData.sum;
+  } else {
+    newSum = previousTransaction.sum;
+  }
+
+  // сума оновленої транзакції з урахуванням знаку (від'ємна для видатків, та позитивна для доходів)
+  let newAmount;
+  if (newType === 'income') {
+    newAmount = newSum;
+  } else {
+    newAmount = -newSum;
+  }
+
+  // зміна балансу: поточний баланс - сума збереженої транзакції + сума оновленої
+  const balanceChange = newAmount - previousAmount;
+  user.balance += balanceChange;
+  await user.save();
+
+  const updatedTransaction = await TransactionCollection.findByIdAndUpdate(
+    transactionId,
+    updateData,
+    { new: true },
+  );
+
+  return updatedTransaction;
 };
 
 export const getSummary = async ({ userId }) => {
