c857de9f0c1d6e971de4a5f5de82bfb1c0a9c3f4
Eugene Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-05-18 19:18:07 +0300

    fix getMonthPeriod

--- src/utils/getMonthPeriod.js
+++ src/utils/getMonthPeriod.js
@@ -2,5 +2,6 @@ export const getMonthPeriod = (year, month) => {
   const beginning = new Date(`${year}-${month}`);
   const from = new Date(beginning);
   const to = new Date(beginning);
+  to.setMonth(month + 1);
   return { $gte: from, $lt: to };
 };
