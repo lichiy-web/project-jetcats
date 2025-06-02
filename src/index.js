b86c98d10fd7d20f1c432e2b9779607e853ef1e4
Yevhen Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-06-02 11:48:11 +0300

    delete consol.logs

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
 import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
-// import { dontSleep } from './utils/ping.js';
 
 const bootstrap = async () => {
-  // dontSleep(10 * ONE_MINUTE);
-  // dontSleep(1000);
+
   await initMongoConnection();
   await createDirIfNotExists(TEMP_UPLOAD_DIR);
   await createDirIfNotExists(UPLOAD_DIR);
